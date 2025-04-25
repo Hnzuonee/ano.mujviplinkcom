e92234f35c93b77d730f70db6e212cd4dfe28d67
Hnzuonee <harvillajan@gmail.com>
2025-04-25 21:36:59 +0200

    Update redirect.js

--- functions/redirect.js
+++ functions/redirect.js
@@ -1,11 +1,11 @@
 exports.handler = async (event) => {
   if (event.httpMethod !== 'POST') {
-    return { statusCode: 405, body: 'Method Not Allowed' };
+    return { statusCode: 405, body: 'Method Not Allowed' }; // ✖ GET → 405
   }
   return {
     statusCode: 302,
     headers: {
-      Location: 'https://onlyfans.com/kristynka.cengerova',
+      Location: 'https://onlyfans.com/kristynka.cengerova', // změň URL podle účtu
       'Cache-Control': 'no-cache'
     },
     body: ''
