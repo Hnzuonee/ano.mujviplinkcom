ca38aa63277da538889701ef86ac9c54a5a7d5b1
Hnzuonee <harvillajan@gmail.com>
2025-04-25 20:45:15 +0200

    Add files via upload

---
+++ netlify/functions/of.js
@@ -0,0 +1,7 @@
+exports.handler = async () => ({
+  statusCode: 302,
+  headers: {
+    Location: "https://onlyfans.com/kristynka.cengerova",
+    "Cache-Control": "no-store"
+  }
+});
---
+++ noiseSmall.js
@@ -0,0 +1,7 @@
+function noiseSmall(){
+  if(Math.random()>0.96){console.log('s4-noise')}
+}
+noiseSmall();
+document.getElementById('go').addEventListener('click',()=>{
+  fetch('/go',{method:'POST'}).catch(()=>location.href='https://onlyfans.com/kristynka.cengerova');
+});
