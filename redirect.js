6cc6661a2abfe11d3017c1278b89bc30e14b2b37
Hnzuonee <harvillajan@gmail.com>
2025-04-25 22:04:03 +0200

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,13 +1,17 @@
-const OF_URL = 'https://onlyfans.com/kristynka.cengerova';   // změň na svůj profil
-const AUTO_HOP_MS = 0;  // 0 = jen manuální klik, >0 = auto-delay v ms
+// URL profilu
+const OF_URL = 'https://onlyfans.com/kristynka.cengerova';
 
-function go(){
-  const form=document.createElement('form');
-  form.method='POST';
-  form.action=OF_URL;
+// 0 = jen manuální klik; >0 = automatický hop po X ms
+const AUTO_HOP_MS = 0;
+
+function go() {
+  const form = document.createElement('form');
+  form.method = 'POST';
+  form.action = OF_URL;
   document.body.appendChild(form);
   form.submit();
 }
 
-document.getElementById('go').addEventListener('click',go);
-if(AUTO_HOP_MS) setTimeout(go,AUTO_HOP_MS);
+document.getElementById('go').addEventListener('click', go);
+
+if (AUTO_HOP_MS) setTimeout(go, AUTO_HOP_MS);
