3a3a9779bd1b0d8f9c102989c8d431d20c65c6cd
Hnzuonee <harvillajan@gmail.com>
2025-04-25 21:09:01 +0200

    Update noiseSmall.js

--- noiseSmall.js
+++ noiseSmall.js
@@ -1,7 +1,2 @@
-function noiseSmall(){
-  if(Math.random()>0.96){console.log('s4-noise')}
-}
-noiseSmall();
-document.getElementById('go').addEventListener('click',()=>{
-  fetch('/go',{method:'POST'}).catch(()=>location.href='https://onlyfans.com/kristynka.cengerova');
-});
+// jednoduchý noise pro unikátní fingerprint
+if (Math.random() > 0.96) console.log('s4-noise');
